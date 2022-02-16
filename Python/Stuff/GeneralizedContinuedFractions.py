c016cc41328a429f4a9b71f86b036e0ccb9f558e
Detsuna <detsuna@gmail.com>
2022-02-17 00:41:23 +0800

    commented out unimplemented fin demo

--- Python/Stuff/GeneralizedContinuedFractions.py
+++ Python/Stuff/GeneralizedContinuedFractions.py
@@ -105,59 +105,54 @@ class NaturalLogDigitGenerator(Stringify) :
         super().__init__(a=a, b=b, **k)
         if neg : self.digits.insert(0, "-")
 
-class LogYofbaseXDigitGenerator(Stringify) : 
-    def __init__(self, *_, z=1, **k) : 
-        a = (lambda k : (2 * (z - 1)) if k==1 else -(((k - 1) * (z - 1))**2))
-        b = (lambda k : 0 if k==0 else ((k + k - 1) * (2 + z - 1)))
-        super().__init__(a=a, b=b, **k)
+# class LogYofbaseXDigitGenerator(Stringify) : 
+#     def __init__(self, *_, z=1, **k) : 
+#         a = (lambda k : (2 * (z - 1)) if k==1 else -(((k - 1) * (z - 1))**2))
+#         b = (lambda k : 0 if k==0 else ((k + k - 1) * (2 + z - 1)))
+#         super().__init__(a=a, b=b, **k)
 
 
 if __name__ == "__main__" : 
     simplefilter('ignore', GeneralizedContinuedFractionValueWarning)
 
-    g = GoldenRatioDigitGenerator(radix=2)
-    for i in range(8097): print(next(g), end="")
+    for cls in [PiDigitGenerator, GoldenRatioDigitGenerator, ExponentialDigitGenerator] : 
+        example = {
+            "generators" : {
+                "base02": cls(radix=2, floatPointChar="."),
+                "base10": cls(radix=10, floatPointChar="."),
+                "base16": cls(radix=16, floatPointChar=".")
+            }, "results" : {
+                "base02": "",
+                "base10": "",
+                "base16": ""
+            }
+        }
+        for i in range(100) : 
+            for key in ["base02", "base10", "base16"] : 
+                example["results"][key] += next(example["generators"][key])
+        print(f"{cls.__name__}\n================")
+        for (k,v) in example["results"].items() : 
+            print(f"{k}: {v}")
+        print()
 
+    print(f"\neˣ\n================")
+    for i in [1, 5, 99] : 
+        print(f"x = {i:3d}: ", end="")
+        g = ExponentialDigitGenerator(z=i, floatPointChar=".")
+        for _ in range(100): print(next(g), end="")
+        print()
 
-
-
-# from Stuff.GeneralizedContinuedFractions import PiDigitGenerator
-
-# example = {
-#     "generators" : {
-#         "base02": PiDigitGenerator(base=2),
-#         "base10": PiDigitGenerator(),
-#         "base16": PiDigitGenerator(base=16)
-#     }, "results" : {
-#         "base02": "",
-#         "base10": "",
-#         "base16": ""
-#     }
-# }
-# for i in range(100):
-#     for key in ["base02", "base10", "base16"] : 
-#         example["results"][key] += next(example["generators"][key])
-# for (k,v) in example["results"].items() : 
-#     print(f"{k}: [{v}]")
-
-
-# π in base 2, 8, 10, 16
-# 11.00100 10000 11111 10110 10101 00010 00100 00101 10100 01100 00100 01101 00110 00100 11000 11001 10001 01000 10111 00000
-# 3.11037 55242 10264 30215 14230 63050 56006 70163 21122 01116 02105 14763 07200 20273 72461 66116 33104 50512 02074 61615
-# 3.14159 26535 89793 23846 26433 83279 50288 41971 69399 37510 58209 74944 59230 78164 06286 20899 86280 34825 34211 70679
-# 3.243F6 A8885 A308D 31319 8A2E0 37073 44A40 93822 299F3 1D008 2EFA9 8EC4E 6C894 52821 E638D 01377 BE546 6CF34 E90C6 CC0AC
-
-# Φ
-# 1.618033988749894848204586834365638117720309179805762862135448622705260462818
-
-# e, e^5, e^99
-# 2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274
-# 148.41315910257660342111558004055227962348766759387898904675284511091206482095857607968840945989902
-# 9889030319346946770560030967138037101405082.60719933517340199715371109444700740600600675067295103719703
-# 
-
-# ln(10)
-# 2.30258509299404568401799145468436420760110148862877297603332790096757260967735248023599720508959829834
-# -0.693147180559945309417232121458176568075500134360255254120680009493393621969694715605863326996418687542
-#  0.693147180559945309417232121458176568075500134360255254120680009493393621969694715605863326996418687542
-# -1.09861228866810969139524523692252570464749055782274945173469433363749429321860896687361575481373208879
+    print(f"\nln(x/y)\n================")
+    print(f" ln(1) : ", end="")
+    g = NaturalLogDigitGenerator(floatPointChar=".")
+    for _ in range(100): print(next(g), end="")
+    print()
+    for i in [2, 3] : 
+        print(f"ln({i}/1): ", end="")
+        g = NaturalLogDigitGenerator(z=i, floatPointChar=".")
+        for _ in range(100): print(next(g), end="")
+        print()
+        print(f"ln(1/{i}): ", end="")
+        g = NaturalLogDigitGenerator(z=(1,i), floatPointChar=".")
+        for _ in range(100): print(next(g), end="")
+        print()
